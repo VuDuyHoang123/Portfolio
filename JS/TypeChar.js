6cc7c320ac94495e8043e6e222c9f335cd3f4dff
VuDuyHoang123 <dtdevil16@gmail.com>
2026-03-07 11:22:06 +0700

    update code

---
+++ JS/TypeChar.js
@@ -0,0 +1,15 @@
+var typed = new Typed('#typed', {
+    strings: [
+        'Data Analyst || Database Administrator || Developer'
+    ],
+    typeSpeed: 50,    // Tốc độ gõ (50-60 là vừa đẹp)
+    loop: false,      // Quan trọng: Tắt vòng lặp để gõ xong là dừng
+    showCursor: true, // Hiện con trỏ để tạo cảm giác đang gõ
+    cursorChar: '|',
+    onComplete: (self) => {
+        // Tùy chọn: Sau khi gõ xong 2 giây thì ẩn con trỏ đi cho sạch giao diện
+        setTimeout(() => {
+            self.cursor.style.display = 'none';
+        }, 2000);
+    }
+});
